992593b666de171d1114279bc0bb694e54695c05
Cong Nguyen <cong.nguyenquoc01@icloud.com>
2023-01-21 19:19:01 +0700

    flutter-cheat-sheet

---
+++ flutter/my_first_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CounterApp());
+}
+
+class CounterApp extends StatelessWidget {
+  const CounterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int counter = 0;
+
+    return MaterialApp();
+  }
+}
