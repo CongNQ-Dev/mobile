b6f69027f42afc2cb782d688c3999a761f5c71a6
Cong Nguyen <cong.nguyenquoc01@icloud.com>
2023-01-30 16:53:11 +0700

    add-button

--- flutter/my_first_app/lib/main.dart
+++ flutter/my_first_app/lib/main.dart
@@ -1,16 +1,52 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(CounterApp());
+  runApp(MyApp());
 }
 
-class CounterApp extends StatelessWidget {
-  const CounterApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    int counter = 0;
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
 
-    return MaterialApp();
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random '),
+          Text(appState.current.asLowerCase),
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed !');
+            },
+            child: Text('Add'),
+          ),
+        ],
+      ),
+    );
   }
 }
